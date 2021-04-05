f023b64a19262d419e9e673d7eb485f4e6ff6f98
KirkNotCaptain <cmariekirk@gmail.com>
2021-04-05 15:41:40 -0400

    Initialized map with route (ny to bos) and waypoint (ny to white plains)

--- src/components/Map/google-map.js
+++ src/components/Map/google-map.js
@@ -9,12 +9,14 @@ class GoogleMap extends Component {
 	renderMap = () => {
 		window.initMap = this.initMap;
 	};
+
 	initMap = () => {
-		let latitude = 40.674;
-		let longitude = -73.945;
+		let latitude = 40.7128;
+		let longitude = -74.0060;
 		var directionsService = new google.maps.DirectionsService();
 		var directionsRenderer = new google.maps.DirectionsRenderer();
-		const map = new window.google.maps.Map(
+		
+    const map = new window.google.maps.Map(
 			document.getElementById("google-map"),
 			{
 				center: new window.google.maps.LatLng(latitude, longitude),
@@ -32,19 +34,44 @@ class GoogleMap extends Component {
 			}
 		);
 
+    directionsRenderer.setMap(map);
+    directionsRenderer.setOptions({
+      preserveViewport: true
+    })
+
+    function calcRoute() {
+      var start = "New York, NY";
+      var end = "Boston, MA"
+      var request = {
+        origin:start,
+        destination:end,
+        waypoints: [{
+          location: { lat: 41.03, lng: -73.76 }
+        }],
+        optimizeWaypoints: true,
+        travelMode: 'DRIVING'
+      };
+      directionsService.route(request, function(response, status) {
+        if (status == 'OK') {
+          directionsRenderer.setDirections(response);
+        }
+      });
+    }
+
+    calcRoute()
+
 		map.addListener("click", (mouseEvent) => {
 			console.log(mouseEvent.latLng.lat());
 			console.log(mouseEvent.latLng.lng());
 		});
 	};
 
-	// `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=30&type=${type}&key=${API_KEY}`
-
 	render() {
 		return (
 			<div className="map-wrapper-container">
 				<div className="map-wrapper">
 					<div className="map-container" id="google-map"></div>
+          <div id="directionsPanel"></div>
 					<Helmet className="helmet">
 						<script
 							type="text/javascript"
