3663d32e23a2ccea2584c480c191e25213022a5c
amenchaca100 <menchacanton@gmail.com>
2021-04-05 11:53:55 -0400

    landing page draft

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import App from "./components/App.jsx";
 import Dashboard from "./components/Trip-Planner/Dashboard.js";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
-
+import LandingPage from './components/LandingPage'
 ReactDOM.render(
 	<Provider store={store}>
-		<Dashboard />
+		<LandingPage />
 	</Provider>,
 	document.getElementById("app")
 );
