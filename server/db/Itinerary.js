6a18964b828bbc19a242cec3ab9ce4b8d1c5983a
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-04-06 18:53:57 -0400

    removed zoom property on double clikc from map

--- server/db/Itinerary.js
+++ server/db/Itinerary.js
@@ -3,7 +3,6 @@ const db = require("./index.js");
 const {model, Schema} = require("mongoose");
 
 const ItinerarySchema = new Schema({
-  // id: {type: Number, unique: true},
   start: Schema.Types.Mixed,
   end: Schema.Types.Mixed,
   waypoints: [Schema.Types.Mixed]
