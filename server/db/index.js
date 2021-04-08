d518e30f1e70f689e65b987bd9efd87071420750
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-04-08 16:52:02 -0400

    ready for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-// const url = 'mongodb://mongo:27017/itinerary';
-const url = "mongodb://localhost/itinerary";
+const url = 'mongodb://mongo:27017/itinerary';
+// const url = "mongodb://localhost/itinerary";
 mongoose.Promise = Promise;
 
 mongoose
