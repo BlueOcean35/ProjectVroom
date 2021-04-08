449c2ccef6b52978874523f9025f461e6835a0ca
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-04-08 18:36:55 -0400

    added console.log to debug server

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.get("/maps/*", function (req, res, next) {
   const url = `https://maps.googleapis.com${req.url}&key=${API_KEY}`;
   axios.get(url)
     .then((response) => {
+      console.log(response);
       res.header("Access-Control-Allow-Origin", "*");
       res.status(response.status).send(response.data.results);
     })
